45081bad4c0d53801f31962f75ff89c0450f00ca
Alex <alejandro.delpuerto2@gmail.com>
2019-04-23 20:50:31 +0200

    Lógica del funcionamiento del menú issue #1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent} from './welcome.component';
-import { HomeComponent} from './home/home.component';
+import { HomeComponent } from './home/home.component';
+import { EventsComponent } from './events/events.component';
+import { FriendsComponent } from './friends/friends.component';
+import { PaymentsComponent } from './payments/payments.component';
+import { RequestsComponent } from './requests/requests.component';
 
 const routes: Routes = [
-  // {path: '', pathMatch: 'full', redirectTo: WelcomeComponent.URL},
-  // {path: WelcomeComponent.URL, component: WelcomeComponent},
-  {
+   // {path: '', pathMatch: 'full', redirectTo: WelcomeComponent.URL},
+   // {path: WelcomeComponent.URL, component: WelcomeComponent},
+   {
     path: HomeComponent.URL, component: HomeComponent,
+    children: [
+      {path: EventsComponent.URL, component: EventsComponent},
+      {path: FriendsComponent.URL, component: FriendsComponent},
+      {path: PaymentsComponent.URL, component: PaymentsComponent},
+      {path: RequestsComponent.URL, component: RequestsComponent}
+    ]
   }
 ];
 
@@ -17,7 +27,11 @@ const routes: Routes = [
 })
 export class AppRoutingModule {
   static COMPONENTS = [
+    EventsComponent,
+    FriendsComponent,
     HomeComponent,
+    PaymentsComponent,
+    RequestsComponent,
     WelcomeComponent
   ];
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {EventsComponent} from '../events/events.component';
+import {FriendsComponent} from '../friends/friends.component';
+import {PaymentsComponent} from '../payments/payments.component';
+import {RequestsComponent} from '../requests/requests.component';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +13,29 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   static URL = 'home';
 
-  constructor() { }
+  constructor(private router: Router) {
+  }
 
   ngOnInit() {
   }
 
+  home() {
+    this.router.navigate([HomeComponent.URL]);
+  }
+
+  events() {
+    this.router.navigate([HomeComponent.URL, EventsComponent.URL]);
+  }
+
+  friends() {
+    this.router.navigate([HomeComponent.URL, FriendsComponent.URL]);
+  }
+
+  payments() {
+    this.router.navigate([HomeComponent.URL, PaymentsComponent.URL]);
+  }
+
+  requests() {
+    this.router.navigate([HomeComponent.URL, RequestsComponent.URL]);
+  }
 }
