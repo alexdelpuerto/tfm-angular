c32b7cd3b4bed53edc8494b2e9997971dc450ce1
Alex <alejandro.delpuerto2@gmail.com>
2019-05-01 15:58:21 +0200

    Diseño de interfaz de ver eventos issue #4

---
+++ src/app/core/crud.component.ts
@@ -0,0 +1,48 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {MatTableDataSource} from '@angular/material';
+
+@Component({
+  selector: 'app-crud',
+  templateUrl: './crud.component.html'
+})
+export class CrudComponent {
+
+  @Input() title = 'Title';
+  @Input() columns: Array<String>;
+  @Input() createAction = true;
+  @Input() readAction = true;
+  @Input() updateAction = true;
+  @Input() deleteAction = true;
+  @Input() editMembersAction = false;
+  @Output() create = new EventEmitter<any>();
+  @Output() read = new EventEmitter<any>();
+  @Output() update = new EventEmitter<any>();
+  @Output() delete = new EventEmitter<any>();
+  @Output() editMembers = new EventEmitter<any>();
+  dataSource: MatTableDataSource<any>;
+
+  @Input()
+  set data(data: any[]) {
+    this.dataSource = new MatTableDataSource<any>(data);
+  }
+
+  onCreate() {
+    this.create.emit();
+  }
+
+  onRead(item) {
+    this.read.emit(item);
+  }
+
+  onUpdate(item) {
+    this.read.emit(item);
+  }
+
+  onDelete(item) {
+    this.delete.emit((item));
+  }
+
+  onEditMembers() {
+    this.editMembers.emit();
+  }
+}
--- src/app/home/events/events.component.ts
+++ src/app/home/events/events.component.ts
@@ -8,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class EventsComponent implements OnInit {
 
   static URL = 'events';
+  data: any;
+  title = 'Eventos';
+  columns = ['nombre', 'presupuesto', 'queda', 'creador'];
+
 
   constructor() {
+    this.data = [
+      {nombre: 'evento1', presupuesto: 23.4, queda: 20, creador: 'pepe'},
+      {nombre: 'evento2', presupuesto: 20.4, queda: 18, creador: 'juan'}];
   }
 
   ngOnInit() {
