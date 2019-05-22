905a99b52cb1c12b330c39fa557d32cf34043593
Alex <alejandro.delpuerto2@gmail.com>
2019-05-22 13:35:13 +0200

    Interfaz buscar amigos issue #12

--- src/app/home/friends/friends.component.ts
+++ src/app/home/friends/friends.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserRegister} from '../users/user-register.model';
 
 @Component({
   selector: 'app-friends',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./friends.component.css']
 })
 export class FriendsComponent implements OnInit {
+
   static URL = 'friends';
-  constructor() { }
+  title = 'Tus Amigos';
+  title2 = 'Añadir Amigos';
+  columns = ['username', 'name', 'surname'];
+  data: any;
+  data2: any;
+  userSearch: UserRegister;
+
+  constructor() {
+    this.userSearch = {username: null, password: null, name: null, surname: null};
+    this.data = [{username: 'user', name: 'name', surname: 'surname'}];
+    this.data2 = [{username: 'nuevo', name: 'name', surname: 'surname'}];
+  }
 
   ngOnInit() {
   }
 
+  resetSearch() {
+    this.userSearch = {username: null, name: null, surname: null, password: null};
+  }
 }
